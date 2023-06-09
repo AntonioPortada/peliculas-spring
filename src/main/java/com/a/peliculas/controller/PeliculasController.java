dffd29927f7e4719161cc3e08dae8c479cd5c4b6
AntonioPortada <antonio_portada25@hotmail.com>
2023-06-09 13:46:24 -0600

    form pelicula y bostrasp

--- src/main/java/com/a/peliculas/controller/PeliculasController.java
+++ src/main/java/com/a/peliculas/controller/PeliculasController.java
@@ -4,17 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.a.peliculas.entity.Pelicula;
+import com.a.peliculas.service.IGeneroService;
 import com.a.peliculas.service.IPeliculaService;
 
 @Controller
 public class PeliculasController {
 
 	private IPeliculaService peliculaService;
+	private IGeneroService generoService;
 	
-	public PeliculasController(IPeliculaService peliculaService) {
+	public PeliculasController(IPeliculaService peliculaService, IGeneroService generoService) {
 		this.peliculaService = peliculaService;
+		this.generoService = generoService;
 	}
 	
 	@GetMapping("pelicula")
@@ -35,8 +39,21 @@ public class PeliculasController {
 		
 		model.addAttribute("titulo", "Editar pelicula");
 		model.addAttribute("pelicula", pelicula);
+		model.addAttribute("generos", generoService.findAll());
 		
 		return "pelicula";
 		
+	}
+	
+	@PostMapping("/pelicula")
+	public String guardar(Pelicula pelicula) {
+		peliculaService.save(pelicula);
+		
+		return "redirect:home";
+	}
+	
+	@GetMapping({"", "/home", "/index"})
+	public String home() {
+		return "home";
 	}
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+//generos
+insert into generos(nombre) values('Comedia');
+insert into generos(nombre) values('Drama');
+insert into generos(nombre) values('Acción');
+insert into generos(nombre) values('Amor');
+
